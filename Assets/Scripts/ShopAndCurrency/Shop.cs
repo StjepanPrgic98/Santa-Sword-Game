6095bb087c3c559bd2d40810157f9af0db33a46c
StjepanPrgic98 <104488741+StjepanPrgic98@users.noreply.github.com>
2023-11-23 23:38:59 +0100

    Add Shop

---
+++ Assets/Scripts/ShopAndCurrency/Shop.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] TextMeshProUGUI description;
+    [SerializeField] TextMeshProUGUI price;
+    [SerializeField] List<Image> levelImages;
+    [SerializeField] Sprite levelUnlockedSprite;
+    [SerializeField] Sprite lockedLevelSprite;
+
+
+    const string multiplyPowerupDescription = "Multiplies presents by 1.5x";
+    const string multiplyRotationSpeedDescription = "Multiplies rotation speed by 1.1x";
+    const string increasePlayerSpeedDescription = "Increases player speed";
+    const string increaseLevelDescription = "Increases present level";
+    const string repairDescription = "Restores Christmas Tree HP by 300";
+
+    public void DisplayPowerupInformation(string powerupName)
+    {
+        switch (powerupName)
+        {
+            case "Multiply":
+                description.text = multiplyPowerupDescription;
+                CheckForPowerupUnlockedLevels(ShopManager.LevelOfPresentMultiplyPowerup);
+                GetPowerupPrice(ShopManager.LevelOfPresentMultiplyPowerup, ShopManager.BasePowerupLevelPrice);
+                break;
+            case "Rotation":
+                description.text = multiplyRotationSpeedDescription;
+                CheckForPowerupUnlockedLevels(ShopManager.LevelOfRotationSpeedPowerup);
+                GetPowerupPrice(ShopManager.LevelOfRotationSpeedPowerup, ShopManager.BasePowerupLevelPrice);
+                break;
+            case "Speed":
+                description.text = increasePlayerSpeedDescription;
+                CheckForPowerupUnlockedLevels(ShopManager.LevelOfPlayerSpeedPowerup);
+                GetPowerupPrice(ShopManager.LevelOfPlayerSpeedPowerup, ShopManager.BasePowerupLevelPrice);
+                break;
+            case "Level":
+                description.text = increaseLevelDescription;
+                CheckForPowerupUnlockedLevels(ShopManager.LevelOfLevelUpPowerup);
+                GetPowerupPrice(ShopManager.LevelOfLevelUpPowerup, ShopManager.BasePowerupLevelPrice);
+                break;
+            case "Repair":
+                description.text = repairDescription;
+                CheckForPowerupUnlockedLevels(ShopManager.LevelOfChristmasTreeRepairPowerup);
+                GetPowerupPrice(ShopManager.LevelOfChristmasTreeRepairPowerup, ShopManager.BasePowerupLevelPrice);
+                break;
+        }
+    }
+
+    void CheckForPowerupUnlockedLevels(int powerupLevel)
+    {
+        foreach (var image in levelImages)
+        {
+            image.sprite = lockedLevelSprite;
+        }
+
+        if(powerupLevel == 0) { return; }
+        for (int i = 0; i < powerupLevel; i++)
+        {
+            levelImages[i].sprite = levelUnlockedSprite;
+        }
+    }
+
+    void GetPowerupPrice(int powerupLevel, int powerupPrice)
+    {
+        int nextLevelPrice = (powerupPrice * powerupLevel) * 2;
+        if(powerupLevel == 0) { nextLevelPrice = ShopManager.BasePowerupLevelPrice; }
+        if(powerupLevel >= 5) { price.text = "Max level!"; return; }
+        price.text = "Next level price: " + nextLevelPrice;
+    }
+}
--- Assets/Scripts/ShopAndCurrency/ShopManager.cs
+++ Assets/Scripts/ShopAndCurrency/ShopManager.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public static class ShopManager
 {
-    public static int LevelOfPresentMultiplyPowerup { get; private set; } = 0;
-    public static int LevelOfRotationSpeedPowerup { get; private set; } = 0;
-    public static int LevelOfPlayerSpeedPowerup { get; private set; } = 0;
-    public static int LevelOfLevelUpPowerup { get; private set; } = 0;
+    public static int LevelOfPresentMultiplyPowerup { get; private set; } = 5;
+    public static int LevelOfRotationSpeedPowerup { get; private set; } = 4;
+    public static int LevelOfPlayerSpeedPowerup { get; private set; } = 3;
+    public static int LevelOfLevelUpPowerup { get; private set; } = 2;
     public static int LevelOfChristmasTreeRepairPowerup { get; private set; } = 0;
+
+    public static int BasePowerupLevelPrice { get; set; } = 50;
 
     public static void IncreaseLevelOfPresentMultiplyPowerup()
     {
